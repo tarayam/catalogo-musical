30f9695fa52160f7c1b05eef7055fd3925d27df6
tarayam <tomas.araya@inacapmail.cl>
2025-09-04 03:00:03 -0400

    fix: asegurar sys.path en wsgi.py para Gunicorn/Render

--- catalogo/wsgi.py
+++ catalogo/wsgi.py
@@ -7,7 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catalogo.settings')
 import os
+import sys
+import pathlib
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR))
+sys.path.insert(0, str(BASE_DIR / 'catalogo'))
 
 from django.core.wsgi import get_wsgi_application
 
